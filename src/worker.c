161247c293031972850e92991384466e0a29600f
Francesco Ciofini <francesco.ciofini@studio.unibo.it>
2025-06-20 16:29:49 +0200

    corretto dimensionamento per il buffer di result dei thread

--- src/worker.c
+++ src/worker.c
@@ -22,7 +22,7 @@ void *build_worker(void *arg_ptr) {
     // di tutti i thread è stata creata dall'init del main)
     FILE* thread_log_fp = fopen(args->thread_log_file, "a");
     if (!thread_log_fp) {
-        char err_buf[256];
+        char err_buf[MAX_CONFIG_ATTR_LEN*2];
         snprintf(err_buf, sizeof(err_buf), "Failed to open thread log file %s: %s", args->thread_log_file, strerror(errno));
         result->error_message = strdup(err_buf);
         pthread_exit(result);
@@ -35,7 +35,7 @@ void *build_worker(void *arg_ptr) {
         
         if (setup_chroot(args, thread_log_fp) != 0) {
             fprintf(thread_log_fp, "Failed to check/setup chroot for %s.", args->arch);
-            char err_buf[256];
+            char err_buf[MAX_CONFIG_ATTR_LEN*2];
             snprintf(err_buf, sizeof(err_buf), "Chroot check/setup failed for %s.", args->arch);
             result->error_message = strdup(err_buf);
             fclose(thread_log_fp);
@@ -47,7 +47,7 @@ void *build_worker(void *arg_ptr) {
         fprintf(thread_log_fp, "Checking and eventually creating worker directories and log file inside chroot for %s.", args->arch);
         if (check_worker_dirs(args, thread_log_fp) != 0) {
             fprintf(thread_log_fp, "Failed to check/create worker directories for %s.", args->arch);
-            char err_buf[256];
+            char err_buf[MAX_CONFIG_ATTR_LEN*2];
             snprintf(err_buf, sizeof(err_buf), "Worker directories check/create failed for %s.", args->arch);
             result->error_message = strdup(err_buf);
             fclose(thread_log_fp);
@@ -64,7 +64,7 @@ void *build_worker(void *arg_ptr) {
     // Non blocco questa operazione con il mutex in quanto andrò solo a copiare lo stesso codice sorgente di sshlirp/libslirp (operazione di lettura)
     if (copy_sources_to_chroot(args, thread_log_fp) != 0) {
         fprintf(thread_log_fp, "Failed to copy sources for %s.", args->arch);
-        char err_buf[256];
+        char err_buf[MAX_CONFIG_ATTR_LEN*2];
         snprintf(err_buf, sizeof(err_buf), "Sources copy failed for %s.", args->arch);
         result->error_message = strdup(err_buf);
         fclose(thread_log_fp);
@@ -80,7 +80,7 @@ void *build_worker(void *arg_ptr) {
         // Eliminazione copie del sorgente anche in caso di fallimento della compilazione
         if (remove_sources_copy_from_chroot(args, thread_log_fp) != 0) {
             fprintf(thread_log_fp, "Error: Failed to remove sources copy for %s.", args->arch);
-            char err_buf[256];
+            char err_buf[MAX_CONFIG_ATTR_LEN*2];
             snprintf(err_buf, sizeof(err_buf), "Failed to remove sources copy after compilation failure for %s.", args->arch);
             result->error_message = strdup(err_buf);
             fclose(thread_log_fp);
@@ -88,7 +88,7 @@ void *build_worker(void *arg_ptr) {
         }
         
         fprintf(thread_log_fp, "Sources copy removed after compilation failure for %s.", args->arch);
-        char err_buf[256];
+        char err_buf[MAX_CONFIG_ATTR_LEN*2];
         snprintf(err_buf, sizeof(err_buf), "Compilation failed for %s.", args->arch);
         result->error_message = strdup(err_buf);
         fclose(thread_log_fp);
@@ -102,7 +102,7 @@ void *build_worker(void *arg_ptr) {
     // Eliminazione copie del sorgente delle sorgenti (operazioni a livello chroot, non richiede mutex)
     if (remove_sources_copy_from_chroot(args, thread_log_fp) != 0) {
         fprintf(thread_log_fp, "Error: Failed to remove sources copy for %s.", args->arch);
-        char err_buf[256];
+        char err_buf[MAX_CONFIG_ATTR_LEN*2];
         snprintf(err_buf, sizeof(err_buf), "Failed to remove sources copy for %s.", args->arch);
         result->error_message = strdup(err_buf);
         fclose(thread_log_fp);
